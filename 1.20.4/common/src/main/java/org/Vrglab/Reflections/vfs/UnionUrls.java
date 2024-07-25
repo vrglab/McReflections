1730db392361479d02be89d47a6ac7c1b9a2d036
Vrglab <vrglab1@gmail.com>
2024-07-25 22:50:33 +0200

    Removed unnecessary code UnionUrls.jar

--- 1.20.4/common/src/main/java/org/Vrglab/Reflections/vfs/UnionUrls.java
+++ 1.20.4/common/src/main/java/org/Vrglab/Reflections/vfs/UnionUrls.java
@@ -1,41 +1,16 @@
 package org.Vrglab.Reflections.vfs;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.DirectoryIteratorException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.jar.JarFile;
 
 public class UnionUrls implements Vfs.Dir {
     private final URL url;
     private final File file;
-    private final List<Path> paths = new ArrayList<Path>();
 
     public UnionUrls(URL url) {
         this.url = url;
         file = new File(url.getFile().split("%")[0]);
-        if(file.exists()) {
-            if(file.isDirectory()) {
-                try (DirectoryStream<Path> stream = Files.newDirectoryStream(file.toPath())) {
-                    for (Path file : stream) {
-                        if(file.getFileName().toString().endsWith(".jar")) {
-                            paths.add(file);
-                        }
-                    }
-                } catch (IOException | DirectoryIteratorException e) {
-                    System.err.println("Error reading directory: " + e.getMessage());
-                }
-            } else {
-                paths.add(file.toPath());
-            }
-        }
     }
 
     @Override
